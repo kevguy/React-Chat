80cac981dc8ff7b4fba2a7d7a93f612f9c7d49c1
kevguy <kevlai22@uw.edu>
2016-11-15 15:56:55 +0800

    finished

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,17 +4,17 @@ import mui from 'material-ui';
 var {ListItem, Avatar} = mui;
 
 class Message extends React.Component {
-	constructor(props){
-		super(props);
-	}
+  constructor(props){
+    super(props);
+  }
 
-	render(){
-		return (
-			<ListItem leftAvatar={<Avatar src="https://up-1.cdn-fullscreendirect.com/production/photos/10272/large/20160818_183851_10272_939474.png" />}>
-				{this.props.message}
-			</ListItem>
-		);
-	}
+  render(){
+    return (
+      <ListItem
+        leftAvatar={<Avatar src={this.props.message.profilePic} />}
+      >{this.props.message.message}</ListItem>
+    );
+  }
 }
 
 export default Message;
